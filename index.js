89e30a4fe787e263d20aa2f6272f5d4a0790784d
Arslan ALAN <alan.arslan@gmail.com>
2018-02-09 15:16:16 +0300

    Change configuration related to Heroku, and change git repo location from parent into "server" folder

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+//On front-end we'll use syntax like --> import express from 'express';
+// But on back-end not, because Node doesn't have support to this syntax
+const app = express();
+
+//Route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+//Defined PORT for Heroku adaptation.
+//This configuration could be change based on service provider like AWS, or digitalocean.
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
