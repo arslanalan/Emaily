422dd3e8b93d623c7dc92dd6a1c0a46d83ddd506
Arslan ALAN <alan.arslan@gmail.com>
2018-02-13 14:04:48 +0300

    Tell express and passport to use cookie for user authentication handling

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 // But on back-end not, because Node doesn't have support to this syntax
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 //Order of models are important, move them to up
 require('./models/User');
@@ -10,6 +12,17 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+//Tell express to use cookie for user state
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+//Tell passport that it should make use of cookies to  handle authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 //"require" returns a function,
 //and we call that function with paramater "app"
