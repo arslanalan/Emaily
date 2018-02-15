02fde7b96647d2059316e2732ed3cc8beb0b4786
Arslan ALAN <alan.arslan@gmail.com>
2018-02-15 15:58:02 +0300

    Creation of auth reducer, and finishing reducer setup

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+//we'll pass reducers to store instead of "() => []"
+import reducers from './reducers';
 
 //Create store with single dumy reducer that returns an array, we're going to replace it
-// and pass an empty object to reducer, it's relevant to back-end,
+//we come back, and changed "() => []" with "reducers"
+// and pass an empty object to store, it's relevant to back-end,
 // so in this case just pass an empty object
-// and finaly pass applyMiddleware() as a thirth parameter
+// and finaly pass applyMiddleware() as a third parameter
 // We don't have any middleware now, we'll come back and modify this
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
     <Provider store={store}>
