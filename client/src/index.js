ea68391ef1d761542a040d42134368807a5ca67d
Arslan ALAN <alan.arslan@gmail.com>
2018-02-16 12:01:56 +0300

    Create Header component and;     - Install materializecss with nmp for styling purposu     - Import materializecss to project     - Style the Header component with materializecss

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import "materialize-css/dist/css/materialize.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
-import App from './components/App';
+import App from "./components/App";
 //we'll pass reducers to store instead of "() => []"
-import reducers from './reducers';
+import reducers from "./reducers";
 
 //Create store with single dumy reducer that returns an array, we're going to replace it
 //we come back, and changed "() => []" with "reducers"
@@ -19,5 +20,5 @@ ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
-    document.querySelector('#root')
+    document.querySelector("#root")
 );
