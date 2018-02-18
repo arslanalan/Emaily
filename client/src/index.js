d40a9311d869a381c7a2f34da363d00dfb83d48c
Arslan ALAN <alan.arslan@gmail.com>
2018-02-18 08:24:31 +0300

    Install axios, and redux-thunk in order to handle "Sign In With Google" in react side Fix editor bug: double quota to single quota

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
-import "materialize-css/dist/css/materialize.min.css";
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-import App from "./components/App";
+import App from './components/App';
 //we'll pass reducers to store instead of "() => []"
-import reducers from "./reducers";
+import reducers from './reducers';
 
 //Create store with single dumy reducer that returns an array, we're going to replace it
 //we come back, and changed "() => []" with "reducers"
@@ -14,11 +15,12 @@ import reducers from "./reducers";
 // so in this case just pass an empty object
 // and finaly pass applyMiddleware() as a third parameter
 // We don't have any middleware now, we'll come back and modify this
-const store = createStore(reducers, {}, applyMiddleware());
+// We come back, and passed reduxThunk as a middleware
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.querySelector("#root")
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
 );
