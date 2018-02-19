e2397131d4dca1d2ce0678e99ba7c3cac414304e
Arslan ALAN <alan.arslan@gmail.com>
2018-02-19 13:59:11 +0300

    Set authReducer return values

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-// If you have one expression in arrow function like this
-// you can remove curly brackets and return keyword
-// instead of function keyword, used arrow function structure
-// no need paranthesis when one argument exist
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user');
 
-    dispatch({ type: FETCH_USER, payload: res });
+    // We just need res.data, nothing else
+    dispatch({ type: FETCH_USER, payload: res.data });
 };
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,11 @@
+import { FETCH_USER } from '../actions/types';
+
 export default function(state = {}, action) {
-    console.log(action);
     switch (action.type) {
+        case FETCH_USER:
+            // If action.payload is empty string return false,
+            // if not return action.payload
+            return action.payload || false;
         default:
             return state;
     }
