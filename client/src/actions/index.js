38cbc9cd1637cbec9f996781c912ae66519f12ee
Arslan ALAN <alan.arslan@gmail.com>
2018-02-19 13:11:39 +0300

    Refactoring of action creator (fetchUser). Using Async/Await structure.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,43 +1,12 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-    // We'll put proxy settings to client\package.json
-    // in order to this relative path get work properly in development envirenment
-    // On production to heroku no need proxy, it'll work properly
+// If you have one expression in arrow function like this
+// you can remove curly brackets and return keyword
+// instead of function keyword, used arrow function structure
+// no need paranthesis when one argument exist
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get('/api/current_user');
 
-    // Rather than returning an action from this action creator when ever the
-    // action creator gets called, it'll instantly return a function
-    // What is the key factor here?
-    // When ww wired up the redux thunk middleware just like src\index.js as shown below;
-    // const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
-    // The purpose of reduxThunk (middleware) is to inspect what ever value we return
-    // from this action creator. If redux thunk sees that we return a function
-    // instead of a normal action, redux thunk will automatically call this function and
-    // pass in that dispatch function as an argument
-    // We can think of as like a big funnel, and anything we toss into it
-    // (any action we toss into it ) will be automatically forwarded off to all the
-    // different reducers in our application
-    return function(dispatch) {
-        // We want to dispatch an action after this request
-        // has been successfully completed
-        // That's the whole point of all this.
-        axios
-            .get('/api/current_user')
-            .then(res => dispatch({ type: FETCH_USER, payload: res }));
-
-        // This is the V-1 of our code. Whenever the action creators called
-        // it will return a function. Redux thunk will see that we return to function
-        // and it will automatically call it with the "dispatch".
-        // We then make a request, we wait until we get the response back from our API
-        // and then once we have the response only at that point in time
-        //  will we actually dispatch our action
-    };
-
-    // We don't use this approach with redux thunk
-    /*const request = axios.get('/api/current_user');
-    return {
-        type: FETCH_USER,
-        payload: request
-    };*/
+    dispatch({ type: FETCH_USER, payload: res });
 };
