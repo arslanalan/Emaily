30cbfb809be4a5986975a5585f81ac41263f810b
Arslan ALAN <alan.arslan@gmail.com>
2018-02-20 15:07:41 +0300

    Defined action creator to send token to server, and dispatch returned user modal

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,6 +4,15 @@ import { FETCH_USER } from './types';
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user');
 
-    // We just need res.data, nothing else
+    dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const handleToken = token => async dispatch => {
+    const res = await axios.post('/api/stripe', token);
+
+    // We assume that '/api/stripe' will return a user model
+    // like '/api/current_user' with updating the credits
+    // We've used the same type, so whenever updated the credits
+    // user have, it'll rerendered
     dispatch({ type: FETCH_USER, payload: res.data });
 };
