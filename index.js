d37660e6ed5e88183a079ef7b783c3cb21086b1b
Arslan ALAN <alan.arslan@gmail.com>
2018-02-27 10:49:49 +0300

    Create Survey model

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
-//Order of models are important, move them to up
+//Order of requires are important, move models to up
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
