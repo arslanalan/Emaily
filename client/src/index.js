65186cacf6a9e1171bf0aa966593e53e58a45bcf
Arslan ALAN <alan.arslan@gmail.com>
2018-02-28 13:24:02 +0300

    Improve email template

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,10 @@ import App from './components/App';
 //we'll pass reducers to store instead of "() => []"
 import reducers from './reducers';
 
+// Development only axios helpers!
+import axios from 'axios';
+window.axios = axios;
+
 //Create store with single dumy reducer that returns an array, we're going to replace it
 //we come back, and changed "() => []" with "reducers"
 // and pass an empty object to store, it's relevant to back-end,
--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,21 @@
 module.exports = survey => {
-    return '<div>' + survey.body + '</div>';
+    // With `` characters we can write multiline html in javascript
+    // and no need escape characters
+    return `
+        <html>
+            <body>
+                <div style="text-align: center;">
+                    <h3>I'd like your input!</h3>
+                    <p>Please answer the following question:</p>
+                    <p>${survey.body}</p>
+                    <div>
+                        <a href="http://localhost:3000">Yes</a>
+                    </div>
+                    <div>
+                        <a href="http://localhost:3000">No</a>
+                    </div>
+                </div>
+            </body>
+        </html>
+    `;
 };
