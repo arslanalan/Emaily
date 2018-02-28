5f80afae8af64ea712e7bf4e9fb9b5cc151d4227
Arslan ALAN <alan.arslan@gmail.com>
2018-02-28 10:38:04 +0300

    Mailer in use with email templates

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -18,6 +20,11 @@ module.exports = app => {
             _user: req.user.id,
             dateSent: Date.now()
         });
+
+        // Great place to send an email!
+        // Mailer: first argument is an object with subject, and recipients argument
+        // second argument is a template with survey.body
+        const mailer = new Mailer(survey, surveyTemplate(survey));
     });
 };
 
