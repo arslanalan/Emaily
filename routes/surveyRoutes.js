af90b16339348302eb7f985ff371018efb2eeb5b
Arslan ALAN <alan.arslan@gmail.com>
2018-02-28 13:57:20 +0300

    Improve route  survey handler

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
 
         const survey = new Survey({
@@ -25,7 +25,19 @@ module.exports = app => {
         // Mailer: first argument is an object with subject, and recipients argument
         // second argument is a template with survey.body
         const mailer = new Mailer(survey, surveyTemplate(survey));
-        mailer.send();
+
+        try {
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+
+            // We send back updated user model
+            // So, Header will be rerendered with the updated credits info
+            res.send(user);
+        } catch (err) {
+            res.status(422).send(err);
+        }
     });
 };
 
