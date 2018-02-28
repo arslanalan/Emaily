39b5873e63ef23e3503babf095349789ecf30f26
Arslan ALAN <alan.arslan@gmail.com>
2018-02-28 12:45:36 +0300

    Cleaned the temporary code

--- client/src/index.js
+++ client/src/index.js
@@ -8,9 +8,6 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 //we'll pass reducers to store instead of "() => []"
 import reducers from './reducers';
-// Temporary code to testing email sending
-import axios from 'axios';
-window.axios = axios;
 
 //Create store with single dumy reducer that returns an array, we're going to replace it
 //we come back, and changed "() => []" with "reducers"
