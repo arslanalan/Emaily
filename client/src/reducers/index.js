4a2bce95a1b4c69dbc70c09ee69e7943c7d6408f
Arslan ALAN <alan.arslan@gmail.com>
2018-02-28 16:29:59 +0300

    Combine redux form reducer automatically created for us by redux-form

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux';
+// import reducer automatically created for us by redux-form
+// we renamed reducer to reduxForm to make more sense
+import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: reduxForm
 });
+
+// "form" is a special key for redux-form, not change it
