e6b9c90a41d0d797e3dab3888153f693de2c2ab8
Arslan ALAN <alan.arslan@gmail.com>
2018-03-01 10:44:42 +0300

    Redux form in practice

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,11 +2,29 @@
 import React, { Component } from 'react';
 // reduxForm helper allows our component to communicate with our redux store
 // like connect helper on react-redux library
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
 class SurveyForm extends Component {
     render() {
-        return <div>SurveyForm!</div>;
+        // When you type text to "surveyTitle" field,
+        // reduxForm takes the value typed, and
+        // keep it in redux store with the key "surveyTitle"
+        return (
+            // handleSubmit() function here is automatically provided to us by reduxForm
+            <div>
+                <form
+                    onSubmit={this.props.handleSubmit(values =>
+                        console.log(values)
+                    )}
+                >
+                    <Field type="text" name="surveyTitle" component="input" />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+            // Instead of using component="input", a standard html input
+            // you would write your own component, like
+            // component="SurveyField", we'll apply this
+        );
     }
 }
 
